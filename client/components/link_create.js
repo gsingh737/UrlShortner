97185165f0baf62ca5741e52a325422704020d65
gsingh737 <ssgurpreetsingh@gmail.com>
2017-07-09 00:29:57 -0400

    Validating url with Check and Match

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -4,10 +4,27 @@
 import React, {Component} from 'react';
 
 class LinkCreate extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: ''
+        };
+    }
+
     handleSubmit(event){
             event.preventDefault();
-
-
+            console.log(this.refs.link.value);
+            Meteor.call('links.insert', this.refs.link.value, (error) => {
+                if(error){
+                    this.setState({error: 'Enter a valid url'});
+                }
+                else {
+                    this.setState({error: ''});
+                    this.refs.link.value = '';
+                }
+                console.log(error);
+            });
     }
 
     render() {
@@ -15,8 +32,9 @@ class LinkCreate extends Component {
             <form onSubmit={this.handleSubmit.bind(this)}>
                 <div className="form-group">
                     <label>Link to shorten </label>
-                    <input className="form-control" />
+                    <input ref="link" className="form-control" />
                 </div>
+                <div className="text-danger">{this.state.error}</div>
                 <button className="btn btn-primary"> Shorten! </button>
             </form>
         );
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -2,5 +2,13 @@
  * Created by User on 7/8/2017.
  */
 import {Mongo} from 'meteor/mongo';
+import validUrl from 'valid-url';
+import {check, Match} from 'meteor/check';
 
+Meteor.methods({
+   'links.insert': function (url) {
+        validUrl.isUri(url);
+        check(url, Match.Where(url => validUrl.isUri(url)));
+   }
+});
 export const Links = new Mongo.Collection('links');
